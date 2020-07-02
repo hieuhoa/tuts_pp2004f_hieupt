635d16c066f12c03d06b26b076f6a2b9251f2295
hieuhoa <hieuhoa1408@gmail.com>
2020-07-02 15:40:56 +0700

    format code

--- app/Ticket.php
+++ app/Ticket.php
@@ -10,14 +10,14 @@ class Ticket extends Model
 
     protected $table = 'tickets';
     protected $guarded = ['id'];
-    protected $fillable    =['title','content','slug','status','user_id'];
+    protected $fillable =['title','content','slug','status','user_id'];
     public function user()
 {
-    return   $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
 }
     public function getTitle()
 {
-    return   $this->title;
+        return $this->title;
 }
 
 }
--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -5,12 +5,12 @@
     <div class="well well bs-component">
         <form class="form-horizontal" method="post">
         @foreach ($errors->all() as $error)
-                <p class="alert alert-danger">{{ $error }}</p>
+            <p class="alert alert-danger">{{ $error }}</p>
         @endforeach
         
-        @if    (session('status'))
-        <div class="alert alert-success">
-                      {{ session('status') }}
+        @if(session('status'))
+            <div class="alert alert-success">
+        {{ session('status') }}
         </div>
         @endif
         <input type="hidden" name="_token" value="{!! csrf_token() !!}">
